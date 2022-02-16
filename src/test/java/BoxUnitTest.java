34db38e15588c589dccde1fd8177f6062654444a
123kant <antoniokuzmenko@gmail.com>
2022-02-16 18:19:08 +0200

    changes made after review

--- src/test/java/BoxUnitTest.java
+++ src/test/java/BoxUnitTest.java
@@ -1,29 +1,37 @@
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class BoxUnitTest {
-    Box box = new Box(3, 4, 5);
-    int box1 = box.squareBox();
-    int box2 = box.perimeterBox();
-    double box3 = box.averageSide();
+    private static int boxSquare;
+    private static int boxPerimeter;
+    private static double boxAverage;
+
+    @BeforeClass
+    public static void setup() {
+        Box box = new Box(3, 4, 5);
+        boxSquare = box.squareBox();
+        boxPerimeter = box.perimeterBox();
+        boxAverage = box.averageSide();
+    }
 
     @Test
     public void testSquare() {
         int vol = 60;
-        Assert.assertEquals(vol, BoxUnitTest.this.box1);
+        Assert.assertEquals(vol, boxSquare);
     }
 
     @Test
     public void testPerimeter() {
         int vol = 12;
-        Assert.assertEquals(vol, BoxUnitTest.this.box2);
+        Assert.assertEquals(vol, boxPerimeter);
     }
 
     @Test
     public void testAverage() {
         double vol = 4.0;
-        Assert.assertEquals(vol, BoxUnitTest.this.box3, 0.01);
+        Assert.assertEquals(vol, boxAverage, 0.01);
     }
 
 }
